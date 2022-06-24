3e2da5c29619f94791fe8a65c22b7a3fd329046e
dmytro.a.frolov <dmytro.a.frolov@globallogic.com>
2022-06-24 17:39:46 +0300

    some

--- src/main/java/com/github/frunoman/spring_boot_tests/SpringBootTestsApplication.java
+++ src/main/java/com/github/frunoman/spring_boot_tests/SpringBootTestsApplication.java
@@ -1,5 +1,6 @@
 package com.github.frunoman.spring_boot_tests;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -32,27 +33,28 @@ public class SpringBootTestsApplication {
         return configurer;
     }
 
-//    @Bean
-//    @Primary
-//    @Scope("thread")
-//    public WebDriver firefoxDriver() {
-//        System.setProperty("webdriver.gecko.driver", this.getClass().getClassLoader().getResource("geckodriver").getPath());
-//        FirefoxDriver driver = new FirefoxDriver();
-//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-//        driver.manage().window().maximize();
-//        return driver;
-//    }
-
     @Bean
+    @Primary
     @Scope("thread")
-    public WebDriver chromeDriver() {
-        System.setProperty("webdriver.chrome.driver", this.getClass().getClassLoader().getResource("chromedriver").getPath());
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--no-sandbox");
-        options.addArguments("--remote-debugging-port="+(9000+new Random().nextInt(500)));
-        ChromeDriver driver = new ChromeDriver(options);
-        driver.manage().window().maximize();
+    public WebDriver firefoxDriver() {
+//        System.setProperty("webdriver.gecko.driver", this.getClass().getClassLoader().getResource("geckodriver").getPath());
+        WebDriverManager.firefoxdriver().setup();
+        FirefoxDriver driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        driver.manage().window().maximize();
         return driver;
     }
+
+//    @Bean
+//    @Scope("thread")
+//    public WebDriver chromeDriver() {
+//        WebDriverManager.chromiumdriver().setup();
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--no-sandbox");
+//        options.addArguments("--remote-debugging-port="+(9000+new Random().nextInt(500)));
+//        WebDriver driver = new ChromeDriver();
+//        driver.manage().window().maximize();
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+//        return driver;
+//    }
 }
